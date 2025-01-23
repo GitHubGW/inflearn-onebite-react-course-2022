df19439c73d9fa83baa1ddef8331b1fd7bd72d64
GW <kowonp@gmail.com>
2025-01-23 16:52:04 +0900

    변수와 상수

---
+++ section01/chapter02.js
@@ -0,0 +1,24 @@
+/**
+ * 변수
+ */
+let age = 20;
+
+age = 30;
+
+// console.log(age);
+
+/**
+ * 상수
+ */
+const birth = "1999-01-01";
+
+birth = "2000-01-01";
+
+/**
+ * 변수 명명 규칙 (네이밍 규칙)
+ */
+let $num = 10;
+let _num = 20;
+let num2 = 30;
+let $2num = 40;
+// let 2num = 50; (x)
