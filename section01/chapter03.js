a8dccfbf81352cb93bb0d731b6340031f594f80c
GW <kowonp@gmail.com>
2025-01-23 16:52:18 +0900

    자료형

---
+++ section01/chapter03.js
@@ -0,0 +1,36 @@
+/**
+ * 자료형
+ */
+
+/**
+ * Number
+ */
+let num1 = 10;
+let num2 = -10;
+let num3 = 1.5;
+let num4 = Infinity;
+let num5 = -Infinity;
+let nan = NaN;
+
+/**
+ * String
+ */
+let str1 = "Hello";
+let str2 = "World";
+let str3 = `Hello ${str1}`;
+
+/**
+ * Boolean
+ */
+let bool1 = true;
+let bool2 = false;
+
+/**
+ * Null
+ */
+let null1 = null;
+
+/**
+ * Undefined
+ */
+let undefined1 = undefined;
