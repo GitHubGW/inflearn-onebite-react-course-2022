0e088bbcc9e857b606700acb43d6983b7c7b51dd
GW <kowonp@gmail.com>
2025-01-27 16:40:35 +0900

    배열

---
+++ section01/chapter15.js
@@ -0,0 +1,15 @@
+/**
+ * 배열
+ */
+
+const arr1 = new Array();
+const arr2 = [];
+
+console.log(arr1);
+console.log(arr2);
+
+let item1 = arr1[0];
+let item2 = arr2[0];
+
+console.log(item1);
+console.log(item2);
